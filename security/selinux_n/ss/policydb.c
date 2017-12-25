6f9f79616dbde473ab68c5ed2f7c24722b468f14
Lukas0610 <mail@lukasberger.at>
2017-12-25 16:52:49 +0100

    selinux: make permissive/enforcing controllable in defconfig

--- security/selinux_n/ss/policydb.c
+++ security/selinux_n/ss/policydb.c
@@ -1501,10 +1501,9 @@ static int type_read(struct policydb *p, struct hashtab *h, void *fp)
 	return 0;
 bad:
 // [ SEC_SELINUX_PORTING_COMMON
-#ifndef CONFIG_ALWAYS_ENFORCE
+#if !defined(SELINUX_ALWAYS_ENFORCE)
 	panic("SELinux:Failed to type read");
-#endif /*CONFIG_ALWAYS_ENFORCE*/
-// ] SEC_SELINUX_PORTING_COMMON
+#endif /*SELINUX_ALWAYS_ENFORCE*/
 	type_destroy(key, typdatum, NULL);
 	return rc;
 }
@@ -2514,10 +2513,9 @@ out:
 	return rc;
 bad:
 // [ SEC_SELINUX_PORTING_COMMON
-#ifndef CONFIG_ALWAYS_ENFORCE
+#if !defined(SELINUX_ALWAYS_ENFORCE)
 	panic("SELinux:Failed to load policy");
-#endif /*CONFIG_ALWAYS_ENFORCE*/
-// ] SEC_SELINUX_PORTING_COMMON
+#endif /*SELINUX_ALWAYS_ENFORCE*/
 	policydb_destroy(p);
 	goto out;
 }
